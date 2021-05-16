36773c6adbb24b176f96b7145ee51b37fcc7e022
Filippo <zzkw35@gmail.com>
2021-05-16 03:08:06 +0200

    Run Black code style
    https://github.com/psf/black

--- api_debugger.py
+++ api_debugger.py
@@ -1,9 +1,12 @@
-#from num2words import num2words
+# from num2words import num2words
 # TODO: Convert numerical input into words in
 # a proper manner, excluding the station number
 import bikemi
 import re
 import unidecode
+
+from geopy.geocoders import Nominatim
+
 
 STATION_INFO = "https://gbfs.urbansharing.com/bikemi.com/station_information.json"
 STATUS_INFO = "https://gbfs.urbansharing.com/bikemi.com/station_status.json"
@@ -12,50 +15,54 @@ api = bikemi.BikeMiApi()
 
 # Search and *PRINT* stations by typing their names or their unique IDs, it's
 # meant to be used with STATION_INFO.
-def findStationPrinter(stations):
-    userInput = input("What station are you searching for? ")
+def find_stationPrinter(stations):
+    user_input = input("What station are you searching for? ")
 
     # Remove accents, all the spaces and special chars from the input
-    userInputEdit = re.sub('[^A-Za-z0-9]+', '', 
-    unidecode.unidecode(userInput))
+    user_inputEdit = re.sub("[^A-Za-z0-9]+", "", unidecode.unidecode(user_input))
 
     for station in stations:
-        # Temporarily treat the station names the same as the userInput
-        stationEdit = re.sub('[^A-Za-z0-9]+', '', 
-        unidecode.unidecode(station['name']))
+        # Temporarily treat the station names the same as the user_input
+        stationEdit = re.sub("[^A-Za-z0-9]+", "", unidecode.unidecode(station["name"]))
 
-        if userInputEdit != ("") and (re.search(userInputEdit, stationEdit,
-        re.IGNORECASE) or re.search(userInput, station['station_id'], re.IGNORECASE)):
+        if user_inputEdit != ("") and (
+            re.search(user_inputEdit, stationEdit, re.IGNORECASE)
+            or re.search(user_input, station["station_id"], re.IGNORECASE)
+        ):
+            locationLink = (
+                "https://www.google.com/maps/search/?api=1&query="
+                + str(station["lat"])
+                + ","
+                + str(station["lon"])
+            )
             print("")
-            print("Name:", station['name'])
-            print("ID:", station['station_id'])
-            print("Address:", station['address'])
-            print("Capacity:", station['capacity'])
-            print("Bikes:", station['bike'])
-            print("Electric Bikes:", station['ebike'])
-            print("Electric Bikes with childseat:", 
-            station['ebike_with_childseat'])
-            print("Available docks:", station['availableDocks'])
-            print("")                
+            print("Name:", station["name"])
+            print("ID:", station["station_id"])
+            print("Address:", station["address"])
+            print("Capacity:", station["capacity"])
+            print("Bikes:", station["bike"])
+            print("Electric Bikes:", station["ebike"])
+            print("Electric Bikes with childseat:", station["ebike_with_childseat"])
+            print("Available docks:", station["availableDocks"])
+            print(locationLink)
+            print("")
             askFullData = input("Do you want to have full data displayed? ")
             if askFullData.lower() == "yes" or askFullData.lower() == "y":
                 print(station)
 
+
 # This class is meant to be used with STATUS_INFO
 def stationStatus(stations):
-    userInput = input("What station are you investigating? -Enter its ID- ")
+    user_input = input("What station are you investigating? -Enter its ID- ")
     for station in stations:
-        if re.search(userInput, station['station_id'], re.IGNORECASE):
+        if re.search(user_input, station["station_id"], re.IGNORECASE):
             print(station)
 
 
-stationsBasicInfo = api.jsonDecoder(STATION_INFO)
-stationsExtraInfo = api.getStationsExtraInfo()
-stationsFullInfo = api.getStationsFullInfo(stationsBasicInfo, stationsExtraInfo)
+get_stations_basic_info = api.json_decoder(STATION_INFO)
+get_stations_extraInfo = api.get_stations_extraInfo()
+stations_full_info = api.get_stations_full_info(
+    get_stations_basic_info, get_stations_extraInfo
+)
 
-print(api.getNearestStation(stationsFullInfo, 45.46472, 9.18577))
-
-#findStationPrinter(stationsFullInfo)
-
-#for station in api.findStation(stationsFullInfo, "cinque"):
-#    distance(station, 45.464100, 9.191926)
+print(find_stationPrinter(stations_full_info))
--- bikemi.py
+++ bikemi.py
@@ -1,30 +1,31 @@
-from math import cos, asin, sqrt, pi
-from operator import itemgetter
-
 import json
 import re
 import requests
 import unidecode
 
+from math import cos, asin, sqrt, pi
+from operator import itemgetter
+
+
 class BikeMiApi:
 
     # Generate a list of stations, stored as dictionaries, starting from the
-    # json files provided by BikeMi at https://bikemi.com/dati-aperti/ 
-    def jsonDecoder(self, InfoUrl):
+    # json files provided by BikeMi at https://bikemi.com/dati-aperti/
+    def json_decoder(self, InfoUrl):
         resp = requests.get(InfoUrl)
         raw = resp.json()
         # Pick the "stations" values inside the "data" key of the "raw" dict
-        stations = raw['data']['stations']
-        # Each station is a dictionary inside the list called "stations"    
-        return(stations)
+        stations = raw["data"]["stations"]
+        # Each station is a dictionary inside the list called "stations"
+        return stations
 
     # Get further info (bike availability) by scraping the website source
-    def getStationsExtraInfo(self):
+    def get_stations_extraInfo(self):
         raw = requests.get("https://bikemi.com/stazioni").text
         placeholder = '"stationMapPage","slug":null},'
         start = raw.find(placeholder) + len(placeholder)
         end = raw.find('"baseUrl":"https://bikemi.com"')
-        stationExtraInfoRaw = raw[start:end].split('DockGroup:')
+        stationExtraInfoRaw = raw[start:end].split("DockGroup:")
         # Each station is a string inside the list called "stationExtraInfo"
         stationExtraInfoList = []
         stationList = []
@@ -36,90 +37,126 @@ class BikeMiApi:
 
             # Create stationList list containing only the relevant data
             # Each station with its data is a list
-            if len(data)==49:
-                stationList.extend((data[1], data[2], data[5], data[6], 
-                data[9], data[10], data[13], data[14], data[18],
-                data[19], data[20], data[21], data[26], data[28],
-                data[32], data[34], data[38], data[40]))
+            if len(data) == 49:
+                stationList.extend(
+                    (
+                        data[1],
+                        data[2],
+                        data[5],
+                        data[6],
+                        data[9],
+                        data[10],
+                        data[13],
+                        data[14],
+                        data[18],
+                        data[19],
+                        data[20],
+                        data[21],
+                        data[26],
+                        data[28],
+                        data[32],
+                        data[34],
+                        data[38],
+                        data[40],
+                    )
+                )
+            if len(data) == 50:  # For the stations with extra address info
+                stationList.extend(
+                    (
+                        data[1],
+                        data[2],
+                        data[5],
+                        data[6],
+                        data[9],
+                        data[10],
+                        data[14],
+                        data[15],
+                        data[19],
+                        data[20],
+                        data[21],
+                        data[22],
+                        data[27],
+                        data[29],
+                        data[33],
+                        data[35],
+                        data[39],
+                        data[41],
+                    )
+                )
 
-            if len(data)==50: # For the stations with extra address info
-                stationList.extend((data[1], data[2], data[5], data[6],
-                data[9], data[10], data[14], data[15], data[19], 
-                data[20],  data[21], data[22], data[27], data[29], 
-                data[33],  data[35],  data[39], data[41]))
-            
-            titles = stationList[::2] # Pick only the data placed in odd positions
-            info = stationList[1::2] # Pick only the data placed in even positions
+            titles = stationList[::2]  # Pick only the data placed in odd positions
+            info = stationList[1::2]  # Pick only the data placed in even positions
 
             # Data cleanup
-            titles = [i.replace('"', '').replace('{', '') for i in titles]
-            
-            info = [i.replace('"', '').replace('}', '')
-            .replace(']', '') for i in info]
-
-            # Parse the data in a dictionary where "titles" are the keys 
+            titles = [i.replace('"', "").replace("{", "") for i in titles]
+            info = [i.replace('"', "").replace("}", "").replace("]", "") for i in info]
+            # Parse the data in a dictionary where "titles" are the keys
             # and "info" are the values
             stationDict = dict(zip(titles, info))
-            
             # Add the newly created dictionary inside a list
-            stationExtraInfoList.append(stationDict) 
+            stationExtraInfoList.append(stationDict)
 
         # Return a list containing all the stations, stored as dictionaries
-        return(stationExtraInfoList)
+        return stationExtraInfoList
 
-    # Merge basic info gathered from the Open Data (json) 
+    # Merge basic info gathered from the Open Data (json)
     # with the extra info scraped from the website
-    def getStationsFullInfo(self, stationsBasicInfo, stationsExtraInfo):
-        stationsBasicInfoSorted = sorted(stationsBasicInfo, 
-        key=itemgetter("station_id"))
-        
-        stationsExtraInfoSorted = sorted(stationsExtraInfo, 
-        key=itemgetter("id"))
-        
-        stationsFullInfo = [a | b for (a, b) in zip(stationsExtraInfoSorted, 
-        stationsBasicInfoSorted)]
-        return(stationsFullInfo)
+    def get_stations_full_info(self, get_stations_basic_info, stations_extra_info):
+        get_stations_basic_infoSorted = sorted(
+            get_stations_basic_info, key=itemgetter("station_id")
+        )
+        stations_extra_infoSorted = sorted(stations_extra_info, key=itemgetter("id"))
+        stations_full_info = [
+            a | b
+            for (a, b) in zip(stations_extra_infoSorted, get_stations_basic_infoSorted)
+        ]
+
+        return stations_full_info
 
     # Search and yield stations by typing their names or their unique IDs,
     # it's meant to be used with STATION_INFO.
-    def findStation(self, stations, userInput):
+    def find_station(self, stations, user_input):
 
         # Remove accents, all the spaces and special chars from the input
-        userInputEdit = re.sub('[^A-Za-z0-9]+', '', 
-        unidecode.unidecode(userInput))
+        user_inputEdit = re.sub("[^A-Za-z0-9]+", "", unidecode.unidecode(user_input))
 
         for station in stations:
-            # Temporarily treat the station names the same as the userInput
-            stationEdit = re.sub('[^A-Za-z0-9]+', '', 
-            unidecode.unidecode(station['name']))
+            # Temporarily treat the station names the same as the user_input
+            stationEdit = re.sub(
+                "[^A-Za-z0-9]+", "", unidecode.unidecode(station["name"])
+            )
 
-            if userInputEdit != ("") and (re.search(userInputEdit, 
-            stationEdit, re.IGNORECASE) or re.search(userInput, 
-            station['station_id'], re.IGNORECASE)):
+            if user_inputEdit != ("") and (
+                re.search(user_inputEdit, stationEdit, re.IGNORECASE)
+                or re.search(user_input, station["station_id"], re.IGNORECASE)
+            ):
                 yield station
 
     # Sort all the stations by chosen key
     def sort(self, stations, key):
         return sorted(stations, key=itemgetter(key))
-    
+
     # Get the nearest station given latitude and longitude
-    def getNearestStation(self, stationsFullInfo, lat, lon):
+    def get_nearest_station(self, stations_full_info, lat, lon):
         distances = []
-        for station in stationsFullInfo:
-            lat2 = station['lat']
-            lon2 = station['lon']
+        for station in stations_full_info:
+            lat2 = station["lat"]
+            lon2 = station["lon"]
 
             # Haversine formula
-            p = pi/180
-            a = (0.5 - cos((lat2-lat)*p)/2 + cos(lat*p) 
-            * cos(lat2*p) * (1-cos((lon2-lon)*p))/2)
-            distance =( 12742 * asin(sqrt(a))) #2*R*asin... km
+            p = pi / 180
+            a = (
+                0.5
+                - cos((lat2 - lat) * p) / 2
+                + cos(lat * p) * cos(lat2 * p) * (1 - cos((lon2 - lon) * p)) / 2
+            )
+            distance = 12742 * asin(sqrt(a))  # 2*R*asin... km
 
             distances.append(distance)
 
-        smallest = min(distances) # Get smallest distance
+        smallest = min(distances)  # Get smallest distance
         # Get index of the specified element in the list
-        # Note: index of the smallest distance = index of 
-        # said station in "stationsFullInfo" list
-        index = distances.index(smallest) 
-        return stationsFullInfo[index]
+        # Note: index of the smallest distance = index of
+        # said station in "stations_full_info" list
+        index = distances.index(smallest)
+        return stations_full_info[index]
